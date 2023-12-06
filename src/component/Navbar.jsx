384877b2fdee16fd8fb05d6e917702571cc17949
leen <leenodeh287@gmail.com>
2023-12-06 18:02:22 +0300

    Using stack component from mui

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,7 +5,7 @@ import { logo } from "../utils/constants";
 import  {SearchBar}  from "../utils/Path";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={50} />
     </Link>
